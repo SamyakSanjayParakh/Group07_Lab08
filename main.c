0d6340e7bf0e84ddbb69428a6b127aa3ad3d89c4
SamyakSanjayParakh <samyakparakh21@gmail.com>
2024-10-17 19:38:43 +0530

    congfiguration for I2C

--- main.c
+++ main.c
@@ -3,7 +3,25 @@
 /**
  * main.c
  */
+#include  <stdint.h>
+
+#include "tm4c123gh6pm.h"
 int main(void)
 {
-	return 0;
+    SYSCTL_RCGCI2C_R =1;
+    SYSCTL_RCGCGPIO_R=2;
+    GPIO_PORTB_AFSEL_R=12;//reviwe
+    GPIO_PORTB_ODR_R=8;//
+    GPIO_PORTB_ODR_R=0x00003300;
+    I2C0_MCR_R=0x00000010;
+    I2C0_MTPR_R=0x00000009;
+    I2C0_MSA_R =0x076;//for 3B
+    I2C0_MDR_R=0X000000EE;
+    I2C0_MCS_R =7;
+
+
+
+
+
+    return 0;
 }
